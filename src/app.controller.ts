fa23b69c23d45f753a2b2213de94f1bb77833670
Morgován Nimród <morgovannimrod22@gmail.com>
2022-12-01 12:36:11 +0100

    Form megírása

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,15 @@
-import { Get, Controller, Render } from '@nestjs/common';
+import { Get, Controller, Render, Body, Post } from '@nestjs/common';
 
 @Controller()
 export class AppController {
   @Get()
-  @Render('index')
+  @Render('zoo')
   root() {
     return {};
+  }
+  @Post()
+  @Render('animal')
+  phoneHandler(@Body() Body: object): object {
+    return Body;
   }
 }
